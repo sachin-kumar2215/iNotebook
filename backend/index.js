5ac55785ea0499200533fcc65a894e5da12e8a8c
sachin kumar <2021ugpi047@nitjsr.ac.in>
2024-02-26 00:36:37 +0530

    f

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,11 @@ app.use(cors())
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
 
+app.get("/",(req,res)=>{
+  res.json("Hello");
+})
 
-
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`iNotebook backend listening on port http://localhost:${port}`)
 })
